62cb2add8b22e3b286012910c3d092bb224d6de8
anastasia <nastya.den1104@gmail.com>
2021-02-21 22:56:12 +0200

    commit

--- build/js/main.js
+++ build/js/main.js
@@ -2,4 +2,37 @@ console.log('test');
 
 $(document).ready(function (){
     $('.banner').slick();
+    $('.slick-prev').addClass('fas fa-chevron-left');
+    $('.slick-next').addClass('fas fa-chevron-right');
+
+    $('.premieres-container').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    // if ($('.slick-list.draggable').closest('.premieres-container .slick-initialized .slick-slider')) {
+    //     $('.slick-list.draggable').addClass('slick-margin');
+    // }
 });

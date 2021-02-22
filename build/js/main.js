c8905cc6922e92007cb4e987484d764d63933a3d
alianora <nastya.den1104@gmail.com>
2021-02-22 16:56:37 +0200

    commit

--- build/js/main.js
+++ build/js/main.js
@@ -32,7 +32,48 @@ $(document).ready(function (){
             }
         ]
     });
-    // if ($('.slick-list.draggable').closest('.premieres-container .slick-initialized .slick-slider')) {
-    //     $('.slick-list.draggable').addClass('slick-margin');
-    // }
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
+
+    var addr = $('.address').text();
+    $(function(){
+
+        var embed= "<iframe class='iframe' width='581' height='401' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?&amp;q="+encodeURIComponent( addr ) + "&amp;output=embed'></iframe>";
+
+        $('.map-frame').html(embed);
+    });
+
+    $(".set > a").on("click", function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+                .siblings(".content")
+                .slideUp(200);
+            $(".set > a i")
+                .removeClass("fa-minus")
+                .addClass("fa-plus");
+        } else {
+            $(".set > a i")
+                .removeClass("fa-minus")
+                .addClass("fa-plus");
+            $(this)
+                .find("i")
+                .removeClass("fa-plus")
+                .addClass("fa-minus");
+            $(".set > a").removeClass("active");
+            $(this).addClass("active");
+            $(".content").slideUp(200);
+            $(this)
+                .siblings(".content")
+                .slideDown(200);
+        }
+    });
 });

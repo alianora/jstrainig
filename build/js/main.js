d30428424f31cba25ae306a7864f06d4b9e51eea
alianora <nastya.den1104@gmail.com>
2021-02-26 16:58:49 +0200

    commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,6 @@
 console.log('test');
 
-$(document).ready(function (){
+$(document).ready(function () {
     $('.banner').slick();
     $('.slick-prev').addClass('fas fa-chevron-left');
     $('.slick-next').addClass('fas fa-chevron-right');
@@ -33,25 +33,25 @@ $(document).ready(function (){
         ]
     });
 
-    $('ul.tabs li').click(function(){
+    $('ul.tabs li').click(function () {
         var tab_id = $(this).attr('data-tab');
 
         $('ul.tabs li').removeClass('current');
         $('.tab-content').removeClass('current');
 
         $(this).addClass('current');
-        $("#"+tab_id).addClass('current');
+        $("#" + tab_id).addClass('current');
     });
 
     var addr = $('.address').text();
-    $(function(){
+    $(function () {
 
-        var embed= "<iframe class='iframe' width='581' height='401' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?&amp;q="+encodeURIComponent( addr ) + "&amp;output=embed'></iframe>";
+        var embed = "<iframe class='iframe' width='581' height='401' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?&amp;q=" + encodeURIComponent(addr) + "&amp;output=embed'></iframe>";
 
         $('.map-frame').html(embed);
     });
 
-    $(".set > a").on("click", function() {
+    $(".set > a").on("click", function () {
         if ($(this).hasClass("active")) {
             $(this).removeClass("active");
             $(this)
@@ -76,4 +76,30 @@ $(document).ready(function (){
                 .slideDown(200);
         }
     });
+
+    $(function () {
+
+        $('li.dropdown > a').on('click', function (event) {
+
+            event.preventDefault();
+            $(this).toggleClass('selected');
+            $(this).parent().find('ul').first().toggle(300);
+
+            $(this).parent().siblings().find('ul').hide(200);
+
+            //Hide menu when clicked outside
+            $(this).parent().find('ul').parent().mouseleave(function () {
+                var thisUI = $(this);
+                $('html').click(function () {
+                    thisUI.children(".dropdown-menu").hide();
+                    thisUI.children("a").removeClass('selected');
+
+                    $('html').unbind('click');
+                });
+            });
+
+        });
+
+    });
+
 });
